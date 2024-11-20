1a6f06d6f008d6d1995cc5f5f8032e0870accdf6
alissonayresmerca <alissonayres.trb@gmail.com>
2024-11-19 22:10:55 -0300

    added tasks array and divider

--- vue2/vuetify-todo/src/views/Todo.vue
+++ vue2/vuetify-todo/src/views/Todo.vue
@@ -1,21 +1,47 @@
 <template>
   <div class="home">
-    <v-list flat>
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active" color="primary"></v-checkbox>
-          </v-list-item-action>
+    <v-list class="pt-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Wake up</v-list-item-title>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
     </v-list>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake up",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Eat breakfast",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Go to work",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
