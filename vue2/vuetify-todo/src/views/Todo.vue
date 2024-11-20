0459146a15d6938ba0511b2e3c7c4657d1ab4b77
alifoo <alissonayres.trb@gmail.com>
2024-11-20 19:11:11 -0300

    added delete button and method

--- vue2/vuetify-todo/src/views/Todo.vue
+++ vue2/vuetify-todo/src/views/Todo.vue
@@ -2,15 +2,26 @@
   <div class="home">
     <v-list class="pt-0" flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item @click="doneTask(task.id)">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
           <template v-slot:default="">
             <v-list-item-action>
               <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
             </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider></v-divider>
@@ -50,6 +61,9 @@ export default {
     doneTask(id) {
       let task = this.tasks.filter((task) => task.id === id)[0];
       task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
 };
