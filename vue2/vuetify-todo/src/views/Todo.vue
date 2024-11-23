a2b26ecd08b3774a41395a943b99a2ab5f9ed47c
alifoo <alissonayres.trb@gmail.com>
2024-11-23 16:21:16 -0300

    moved list tasks, field add tasks and individual tasks into child components

--- vue2/vuetify-todo/src/views/Todo.vue
+++ vue2/vuetify-todo/src/views/Todo.vue
@@ -1,43 +1,7 @@
 <template>
   <div class="home">
-    <v-text-field
-      v-model="newTaskTitle"
-      @click:append="addTask"
-      @keyup.enter="addTask"
-      class="pa-3"
-      outlined
-      label="Add task"
-      append-icon="mdi-plus"
-      hide-details
-      clearable
-    ></v-text-field>
-    <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
-      <div v-for="task in $store.state.tasks" :key="task.id">
-        <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5': task.done }"
-        >
-          <template v-slot:default="">
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-                >{{ task.title }}</v-list-item-title
-              >
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn icon @click.stop="$store.commit('deleteTask', task.id)">
-                <v-icon color="primary lighten-1">mdi-delete</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-    </v-list>
+    <field-add-task />
+    <list-tasks v-if="$store.state.tasks.length" />
     <div v-else class="no-tasks">
       <v-icon size="100" color="primary"> mdi-check </v-icon>
       <p class="text-h5 primary--text">No tasks</p>
@@ -46,18 +10,14 @@
 </template>
 
 <script>
+import FieldAddTask from "../components/Todo/FieldAddTask.vue";
+import ListTasks from "@/components/Todo/ListTasks.vue";
+
 export default {
   name: "Home",
-  data() {
-    return {
-      newTaskTitle: "",
-    };
-  },
-  methods: {
-    addTask() {
-      this.$store.commit("addTask", this.newTaskTitle);
-      this.newTaskTitle = "";
-    },
+  components: {
+    "field-add-task": FieldAddTask,
+    "list-tasks": ListTasks,
   },
 };
 </script>
